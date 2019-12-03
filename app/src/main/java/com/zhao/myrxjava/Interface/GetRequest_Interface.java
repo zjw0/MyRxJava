a730a29f51c1004897733326533777948becdbd7
jack_zhao <15038707891@163.com>
2019-12-03 18:00:23 +0800

    RxjavaAndRetrofit

--- app/src/main/java/com/zhao/myrxjava/Interface/GetRequest_Interface.java
+++ app/src/main/java/com/zhao/myrxjava/Interface/GetRequest_Interface.java
@@ -2,6 +2,7 @@ package com.zhao.myrxjava.Interface;
 
 import com.zhao.myrxjava.bean.TranslationBean;
 
+import io.reactivex.Observable;
 import retrofit2.Call;
 import retrofit2.http.GET;
 
@@ -9,12 +10,13 @@ import retrofit2.http.GET;
  * Created by YBD-TECH029 on 2019/12/3.
  */
 
-public interface GetRequestInterface {
+public interface GetRequest_Interface {
 
     @GET("ajax.php?a=fy&f=auto&t=auto&w=hi%20world")
-    Call<TranslationBean> getCall();
+    Observable<TranslationBean> getCall();
     // 注解里传入 网络请求 的部分URL地址
     // Retrofit把网络请求的URL分成了两部分：一部分放在Retrofit对象里，另一部分放在网络请求接口里
     // 如果接口里的url是一个完整的网址，那么放在Retrofit对象里的URL可以忽略
+    // 采用Observable<...>接口
     // getCall()是接受网络请求数据的方法
 }
--- app/src/main/java/com/zhao/myrxjava/MainActivity.java
+++ app/src/main/java/com/zhao/myrxjava/MainActivity.java
@@ -7,6 +7,10 @@ import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
+import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import com.zhao.myrxjava.Interface.GetRequestInterface;
+import com.zhao.myrxjava.Interface.GetRequest_Interface;
+import com.zhao.myrxjava.bean.TranslationBean;
 import com.zhao.myrxjava.net.GetRequest;
 import com.zhao.myrxjava.net.PostRequest;
 
@@ -14,7 +18,11 @@ import io.reactivex.Observable;
 import io.reactivex.ObservableEmitter;
 import io.reactivex.ObservableOnSubscribe;
 import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,6 +32,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rxJavaRetrofit();
+
         findViewById(R.id.tv_get).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -136,5 +147,46 @@ public class MainActivity extends AppCompatActivity {
             }
 
         });
+    }
+
+    private void rxJavaRetrofit() {
+        //步骤4：创建Retrofit对象
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://fy.iciba.com/") // 设置 网络请求 Url
+                .addConverterFactory(GsonConverterFactory.create()) //设置使用Gson解析(记得加入依赖)
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create()) // 支持RxJava
+                .build();
+
+        // 步骤5：创建 网络请求接口 的实例
+        GetRequest_Interface request = retrofit.create(GetRequest_Interface.class);
+
+        // 步骤6：采用Observable<...>形式 对 网络请求 进行封装
+        Observable<TranslationBean> observable = request.getCall();
+
+        // 步骤7：发送网络请求
+        observable.subscribeOn(Schedulers.io())               // 在IO线程进行网络请求
+                .observeOn(AndroidSchedulers.mainThread())  // 回到主线程 处理请求结果
+                .subscribe(new Observer<TranslationBean>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+                        Log.d(TAG, "开始采用subscribe连接");
+                    }
+
+                    @Override
+                    public void onNext(TranslationBean result) {
+                        // 步骤8：对返回的数据进行处理
+                        result.show() ;
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Log.d(TAG, "请求失败");
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        Log.d(TAG, "请求成功");
+                    }
+                });
     }
 }
